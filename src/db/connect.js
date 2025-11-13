f074247c36e563982272408d9e635d0706e7e6e0
PedroPsousilva <pedro.soupsilva@gmail.com>
2025-11-13 18:32:28 -0300

    alteerando o db3

--- src/db/connect.js
+++ src/db/connect.js
@@ -6,10 +6,5 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  charset: 'utf8mb4'
+ 
 });
-
-
-const db = pool.promise();
-
-module.exports = db;
