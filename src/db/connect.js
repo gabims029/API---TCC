fc5ec05a3b995b696c470d79ba24ef6830711d01
PedroPsousilva <pedro.soupsilva@gmail.com>
2025-11-13 18:03:31 -0300

    alteerando o db

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    connectionLimit: 10, 
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USER, 
-    password: process.env.DB_PASSWORD, 
-    database: process.env.DB_NAME 
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  charset: 'utf8mb4' // 
 });
-
-module.exports = pool;
