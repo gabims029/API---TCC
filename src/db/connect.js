b03067aa114bdd28cdb16022c8ee000cd3b0f44c
PedroPsousilva <pedro.soupsilva@gmail.com>
2025-11-16 11:14:48 -0300

    alteerando o db para uncionar

--- src/db/connect.js
+++ src/db/connect.js
@@ -6,15 +6,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  charset: 'utf8mb4',
-  supportBigNumbers: true
-});
-
-// 👉 Garantir UTF-8 em toda nova conexão
-pool.on('connection', function (connection) {
-  connection.query("SET NAMES utf8mb4;");
-  connection.query("SET CHARACTER SET utf8mb4;");
-  connection.query("SET SESSION collation_connection = 'utf8mb4_unicode_ci';");
+  charset: 'utf8mb4'
 });
 
 module.exports = pool;
